88a32422743b58e3d8354d9d4bcaceae96e455cd
nx-xn2002 <1870230468@qq.com>
2024-02-22 14:26:44 +0800

    服务提供者实现

---
+++ example-provider/src/main/java/com/nx/example/provider/EasyProviderExample.java
@@ -0,0 +1,12 @@
+package com.nx.example.provider;
+
+/**
+ * 服务提供者启动类
+ *
+ * @author nx
+ */
+public class EasyProviderExample {
+    public static void main(String[] args) {
+        //TODO: 提供服务
+    }
+}
