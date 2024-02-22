c1510fe00b12dee1d826066ac9796985d0d5f2b0
nx-xn2002 <1870230468@qq.com>
2024-02-22 14:17:39 +0800

    配置公共模块

--- example-common/src/main/java/com/nx/example/common/model/User.java
+++ example-common/src/main/java/com/nx/example/common/model/User.java
@@ -1,2 +1,20 @@
-package com.nx.example.common.model;public class User {
+package com.nx.example.common.model;
+
+import java.io.Serializable;
+
+/**
+ * 用户实体类
+ *
+ * @author nx
+ */
+public class User implements Serializable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- example-common/src/main/java/com/nx/example/common/service/UserService.java
+++ example-common/src/main/java/com/nx/example/common/service/UserService.java
@@ -1,2 +1,19 @@
-package com.nx.example.common.service;public interface UserService {
+package com.nx.example.common.service;
+
+import com.nx.example.common.model.User;
+
+/**
+ * 用户服务接口
+ *
+ * @author nx
+ */
+public interface UserService {
+    /**
+     * 获取用户
+     *
+     * @param user
+     * @return {@link User }
+     * @author nx
+     */
+    User getUser(User user);
 }
